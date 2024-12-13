781c7b46df9cab35b60c2f677bd871ebddbfa302
Cousprr <cousprr3517@163.com>
2024-12-13 19:12:48 +0800

    schedule version1; not solved conbination

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,10 +1,11 @@
 class ScheduleController < ApplicationController
   def index
     @course=current_user.courses if student_logged_in?
+    @time_code=[]
+    @week_code=[]
     @course.each do |course|
-      match=course.course_time.match(/\((\d+-\d+)\)/)
-      @time_code=match.split("-").join("")
-      @week_code=course.course_time
+      @time_code<<course.course_time[3,1].to_i
+      @week_code<<course.course_time[0,2]
     end
   end
 end
